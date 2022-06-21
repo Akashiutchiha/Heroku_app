3e9e5331360cdbd4e75088361c6d3b7160f203f1
Ndongmo Christian <christianhonore2003@gmail.com>
2022-06-21 10:45:11 +0100

    Django default

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps',
+    'main.apps.MainConfig',
     'django_on_heroku'
 ]
 
